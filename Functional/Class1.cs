ab0b1e639fef8f41d240cfb0164047ec8bd04a7e
phillyai <sonagi.philly@gmail.com>
2018-02-03 01:07:33 +0900

    커링 초기 커밋

--- Functional/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Functional
-{
-    public class Class1
-    {
-    }
-}
---
+++ Functional/Curry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Functional
+{
+    public class Curry<T>
+    {
+        private Curry()
+        {
+
+        }
+
+        public static Curry<T> Make<T>(T function)
+        {
+
+        }
+    }
+}
